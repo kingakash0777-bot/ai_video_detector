57e89fe314870b6d656563924fb8b2ed4ebeedad
Akash <kingakash0777@gmail.com>
2026-03-30 20:30:35 +0530

    Initial backend with AI detection

---
+++ create_model.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import os
+
+# ensure model folder exists
+os.makedirs("model", exist_ok=True)
+
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu', input_shape=(224,224,3)),
+    layers.MaxPooling2D(2,2),
+
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D(2,2),
+
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+model.save("model/deepfake_model.h5")
+
+print("✅ Model created successfully!")
